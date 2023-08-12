9275b3b8dff43f94322c3b5899d165878a62cffa
Geolidth <myyouz74@gmail.com>
2023-08-12 16:19:17 +0200

    Felesleges importok kiszedése a *component.css fájlokból, méretcsökkentés

--- src/app/bulma-card/bulma-card.component.ts
+++ src/app/bulma-card/bulma-card.component.ts
@@ -16,8 +16,8 @@ export class BulmaCardComponent implements OnInit {
   ngOnInit(): void {
     const filePath = "../assets/konyvek.json";
     this.jsonReadService.readJsonFile(filePath).subscribe({
-      next:resp =>this.jsonData=resp,
-      error:err=>console.error(err)
+      next: resp => this.jsonData = resp,
+      error: err => console.error(err)
     });
   }
 }
