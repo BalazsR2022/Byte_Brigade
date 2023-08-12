78d4af347f5dbdc3c08ffa4d21e77d5a5ed6cffe
Geolidth <myyouz74@gmail.com>
2023-08-12 10:06:04 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatCardModule } from "@angular/material/card";
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BulmaCardComponent } from './bulma-card/bulma-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BulmaCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bulma-card/bulma-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bulma-card',
+  templateUrl: './bulma-card.component.html',
+  styleUrls: ['./bulma-card.component.css']
+})
+export class BulmaCardComponent {
+
+}
