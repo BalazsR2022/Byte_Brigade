d5cdf4c5c60fee28767de4daa3175e7a5e492675
Geolidth <myyouz74@gmail.com>
2023-11-12 14:56:23 +0100

    Navbar komponens javítása mobilnézetben

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  ngOnInit() {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        console.log("Megnyomtál!");
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        el.classList.toggle('is-active');
+        if ($target) {
+          $target.classList.toggle('is-active');
+        }
+      });
+    });
+  }
 }
