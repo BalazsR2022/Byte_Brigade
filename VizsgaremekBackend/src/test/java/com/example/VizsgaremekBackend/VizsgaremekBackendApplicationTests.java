ad06fb7e26e0f38bc62c03059f226c2926c33f0b
Balázs Réka <balazs.reka@ktch.hu>
2024-01-21 18:46:43 +0100

    Spring hozzáadása, kép formázása surprise oldalon

---
+++ VizsgaremekBackend/src/test/java/com/example/VizsgaremekBackend/VizsgaremekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VizsgaremekBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizsgaremekBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
