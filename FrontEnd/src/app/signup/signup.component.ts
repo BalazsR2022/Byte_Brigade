2acacc9db6df6e09bb2800c946e7555ff3216ec2
Csajbók-Reményi László <myyouz74@gmail.com>
2024-03-17 13:52:11 +0100

    userForm reset

--- FrontEnd/src/app/signup/signup.component.ts
+++ FrontEnd/src/app/signup/signup.component.ts
@@ -7,25 +7,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  userForm: FormGroup;
+  userForm: any;
   userFormValid:boolean=true;
   constructor(){
+    this.initUserForm();
+  }
+  
+  saveUserData(){
+    if(this.userForm.valid){
+      console.log(this.userForm);
+      this.initUserForm();
+    }else{
+      this.userFormValid=false;
+    }
+  }
 
+  initUserForm(){
     this.userForm = new FormGroup({
       name: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
       county: new FormControl('Megyék', Validators.required)
     });
-
-  }
-  
-  saveUserData(){
-    if(this.userForm.valid){
-      console.log(this.userForm);
-    }else{
-      this.userFormValid=false;
-    }
   }
 
 }
