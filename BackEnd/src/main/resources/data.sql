391ca93a31742e804860c0b704bc18ed8b1eee90
Csajbók-Reményi László <myyouz74@gmail.com>
2024-04-19 17:43:33 +0200

    Nyelvi készlet javítva

--- BackEnd/src/main/resources/data.sql
+++ BackEnd/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS bookdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS bookdb CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
 
 USE bookdb;
 
