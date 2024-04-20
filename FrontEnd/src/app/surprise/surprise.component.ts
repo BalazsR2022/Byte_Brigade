2dca328a2056decc5a277c45d283f77a854c9c8e
Csajbók-Reményi László <myyouz74@gmail.com>
2024-04-20 12:29:23 +0200

    Surprise adattöltés OK

--- FrontEnd/src/app/surprise/surprise.component.ts
+++ FrontEnd/src/app/surprise/surprise.component.ts
@@ -1,46 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseService } from '../base.service';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-surprise',
   templateUrl: './surprise.component.html',
   styleUrls: ['./surprise.component.css']
 })
-export class SurpriseComponent implements OnInit  {
+export class SurpriseComponent  {
+  url='http://localhost:8080/'
   object:any={};
-  arrayBook:any=[];
+  arrayBook:any=new BehaviorSubject<any>(null);
   thisBook:any={};
   emptyData = false;
   clicked = false;
   rand!: number;
   image:any="regény";
-  constructor(private base: BaseService) { }
-
-  ngOnInit (): void {
-    this.clicked = false;
+  constructor(private base: BaseService, private http: HttpClient) { 
     this.initBooks();
+    
   }
 
   initBooks() {
-    this.base.getBooks().subscribe({
-      next: (res) => {
-        this.object=res;
-        this.arrayBook=res;
-        this.emptyData = false;
-      },
-      error: (err) => {
-        console.log('Hiba az oldalon: ' + err);
-        this.emptyData = true;
-      }
-    });
+    let urlGet=this.url+"guest/books";
+     this.http.get(urlGet).subscribe((data)=>{
+       this.arrayBook.next(data);
+       console.log(data);
+     });
+     
   }
 
   loadSurprise() {
     if (!this.clicked){ 
       this.clicked = true;
     }
-    this.rand = this.getRandomInt(0, (this.arrayBook.length - 1));
-    this.object=this.arrayBook[this.rand];
+    this.rand = this.getRandomInt(0, (this.arrayBook.getValue().length - 1));
+    this.object=this.arrayBook.getValue()[this.rand];
     this.image=this.object.category;
   }
 
